36b8373bf2abbf3e5e1b28a593286ca3a1776625
Vincent Manera <vincent.manera@squiz.net>
2018-10-17 19:38:00 +0800

    code splitting test case

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import "./styles/global.scss";
 
-// console.log('hello')
+// import $ from "jquery";
+
+// import _ from 'lodash';
+
+//code splitting sample
+function getComponent() {
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+        var element = document.createElement('div');
+
+        element.innerHTML = _.join(['codesplit', 'functioning'], ' ');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
+}
+
+getComponent().then(component => {
+    document.body.appendChild(component);
+});
